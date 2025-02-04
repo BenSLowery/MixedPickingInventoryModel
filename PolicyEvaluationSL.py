5f163bfe1c0281c2732145fe79e1cbe8c5c41c08
BenSLowery <benjaminslowery@outlook.com>
2025-02-04 09:57:51 +0000

    new file:   PolicyEvaluationSL.py 	new file:   PolicyEvaluation_waste_shortage.py 	new file:   test_SL_pol.py 	deleted:    PolicyEvaluation.py 	deleted:    PolicyEvaluationRandomPicking.py 	deleted:    PolicyEvaluationSL.py 	deleted:    PolicyEvaluation_waste_shortage.py 	deleted:    binom_cython.c 	deleted:    binom_cython.pyx 	deleted:    perishable_optimal.py 	deleted:    setup.py 	deleted:    test_SL_pol.py 	deleted:    test_m_3.py 	deleted:    test_pol_eval.py 	deleted:    test_pol_random_picking.py

---
+++ PolicyEvaluationSL.py
@@ -0,0 +1,206 @@
+# Policy evaluation for SL
+
+import numpy as np
+import scipy.stats as sp
+import binom_cython
+import itertools
+import pickle
+import uuid
+import multiprocessing as mp
+import math
+from decimal import Decimal
+
+# Optimal perishing class
+class PolicyEvaluationSL():
+
+    def __init__(self, periods, lifetime, holding_c, penalty_c, order_c, outdating_c, perish_prob, discount_factor, demand_params, demand_truncation, num_cores, FIFO_rate):
+        
+        # Assign inputs
+        self.T = periods
+        self.m = lifetime
+        self.h = holding_c
+        self.p = penalty_c
+        self.c = order_c
+        self.theta = outdating_c
+        self.psi = perish_prob
+        self.gamma = discount_factor
+        self.params = demand_params # Assumed stationary, can be poisson or negative binomial atm
+        self.max_d = demand_truncation
+        self.demand_val, self.demand_pmf = self.gen_demand()
+        self.fifo_rate = FIFO_rate # Rate at which we assume customers are fifo/lifo in the simulation, used for the calc_post_demand() function
+
+        self.save_output = True # Assume we can save output to pickle file, might want to set to false if running small time tests but need to do so manually
+
+
+        # Save the number of cpus to use when parallising
+        self.num_cores = num_cores
+
+
+        # Store states and answers
+
+        self.V = [] # Previous states
+        
+        if self.params[0] == 'Poisson':
+            self.distr_cdf = {x: sp.poisson(self.params[1][0]).cdf(x) for x in range(self.max_d*self.m)}
+        elif self.params[0] == 'NegBin':     
+            # Generate new random negative binomial 
+            self.distr_cdf = {x: sp.nbinom(self.params[1][0], self.params[1][1]).cdf(x) for x in range(self.max_d*self.m)}
+
+        ########################
+        # Pre-processing steps #
+        ########################
+
+        # Generate the state space
+        self.state_space = [x for x in itertools.product(*[[i for i in range(self.max_d)] for j in range(self.m)])]
+
+        # Pre-calculare the cost functions, parellised
+        self.G = {}
+
+        # Calculated expected shortage and waste from the function
+        self.exp_shortage = {}
+        self.exp_waste = {}
+        print('Pre-calculating cost function... ', end=' ')
+        
+        cl=mp.Pool(num_cores)
+        # Map the calculation of the cost function. State space ar eall independent so can be computed in parallel
+        res = cl.map(self.calc_immediate_cost,self.state_space)
+        # Save to dictionary to call upon later
+        for idx, x in enumerate(self.state_space):
+            self.G[x] = res[idx]
+        # Close the process.
+        cl.close()
+        print('Done! \n')
+
+
+    def obj_func(self,x):
+        return 1 if x >= 0 else 0
+    
+    def gen_demand(self):
+        # Check which distribution we are checking
+        if self.params[0] == 'Poisson':
+            distr = sp.poisson(self.params[1][0])
+
+        elif self.params[0] == 'NegBin':     
+            # Generate new random negative binomial 
+            distr = sp.nbinom(self.params[1][0], self.params[1][1])
+        
+        # Generate value/pmf pairings
+        vals = [i for i in range(self.max_d)]
+        pmf = [distr.pmf(v) for v in vals]
+        return vals, pmf
+    
+    
+    def calc_post_demand(self,x,d):
+        """
+            Calculates the post demand state according to LIFO and FIFO as a function of demand
+            i.e. in Clarkson (2022) its the y_{t,i}(d_t)
+        """
+
+        y = []
+
+        # Get split
+        FIFO_d = round(self.fifo_rate*d)
+        LIFO_d = d-FIFO_d
+        
+        # Realise FIFO
+        for i in range(self.m):
+            x_rest = sum([x[j] for j in range(i+1,self.m)])
+            y.append(max(x[i]-max(FIFO_d-x_rest,0),0))
+        
+        # Realise LIFO
+        for i in range(1,self.m+1):
+            y_rest = sum([x[j] for j in range(i-1)])
+            y[i-1] = max(y[i-1]-max(LIFO_d-y_rest,0),0)
+        
+        return y
+
+    def terminal_state(self):
+        """
+            Appends the terminal condition for the SDP - V_{T+1} in notation, see Eq. 8 in Clarkson et al.(2022)
+        """
+        # Assign terminal cost (which would just be 1 as we satisfy all demand at the end as theres.... no demand.)
+        for x in self.state_space:
+            self.V.append((list(x), 1))
+        return
+
+    def calc_immediate_cost(self, x):
+        return self.distr_cdf[np.sum(x)]
+
+    
+    def calc_perish_combos(self, x, d):
+        """
+            Calculate the j perishing combinations set, and returns the post demand state too
+        """
+        y_t = self.calc_post_demand(x,d) # Calculate post demand state
+        
+        # generate set, ignore the last demand class, might be a bit messy with list comprehension but seemed most intuitive way
+        j_d = [j for j in itertools.product(*[[j_i for j_i in range(int(y_i)+1)] for y_i in y_t[:-1]])]
+
+        return y_t, j_d
+    
+    # This only works for m=3 due to the line
+    # func_val_prod = binom_perish_f[0]*binom_perish_f[1]
+    def calc_future_cost(self,x, q, V_t_plus_1):
+        fut_cost = 0
+        for d in range(self.max_d):
+            # Calculate the set of inventory combinations after perishing, and also keep post demand state for use in pmf calculation
+            y_t, j_d = self.calc_perish_combos(x,d)
+            inner_fut_cost = 0
+
+            for j in j_d: # Iterate through perishing combinations
+                
+                binom_perish_f = []
+                for i in range(self.m-1): # each demand class
+
+                    binom_perish_f.append(binom_cython.binom_cython(j[i], y_t[i], 1-self.psi[i])) # call cython function to calculate pmf
+                func_val_prod = binom_perish_f[0]*binom_perish_f[1]
+                inner_fut_cost+=func_val_prod*V_t_plus_1[(q,)+j]
+            # Multiply inner cost function by the demand pmf
+            fut_cost += inner_fut_cost*self.demand_pmf[d]
+
+        return fut_cost
+
+    def calculate_cost_to_go(self, x,q, V_t_plus_1):
+
+
+        im_cost = self.G[x]
+
+        fut = self.calc_future_cost(x,q,V_t_plus_1)
+        fut_cost = self.gamma*fut
+
+        total_cost = im_cost+fut_cost
+
+        return [x, total_cost]
+
+    def EvalPolicy(self, policy):
+        
+        # Step 1. Iterate backwards recursively through all periods
+        for period in range(self.T,0,-1):
+            print('Period: {}'.format(period))
+            # Keep future value functions
+            V_t_plus_1 = {tuple(val_func[0]):val_func[1] for val_func in self.V}
+            self.V = []
+            
+            # Get the set of actions/ordering decisions in this period
+            policy_t = policy[policy['period'] == period]
+
+            # Step 2. Enumerate through all the possible inventory levels
+            # Parellelised
+            cl = mp.Pool(self.num_cores)
+            x_v_duos = cl.starmap(self.calculate_cost_to_go, list(zip(policy_t.Inventory.values,policy_t.Order.values,itertools.repeat(V_t_plus_1))))
+            cl.close()
+
+            # Save optimal policy and V_{t+1} function
+            for x,v in x_v_duos:
+                self.V.append((x,v))
+
+            
+        if(self.save_output):
+            ext = self.params[0] + str(self.p) + str(self.theta)
+
+            file = open('/beegfs/client/default/loweryb/sustainability/evaluation/SL/' + ext + '/testing_sl_rate_' + str(self.fifo_rate)  +'_with_policy_'+ str(self.input_policy_rate)  + '.pkl','wb')
+            file.write(pickle.dumps(self.V))
+            file.close()
+
+            print("Object Saved!")
+ 
